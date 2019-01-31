6a84213bc0a79fc2a0fc3607b50fd2a2cebbb9dd
dmalvi <dmalvi@gmail.com>
2019-02-01 00:45:15 +0100

    new copy

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Aspside />
+  </div>
+</template>
+
+<script>
+import Aspside from './components/Aspside'
+
+export default {
+  name: 'app',
+  components: {
+    Aspside
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Segoe UI','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* color: #2c3e50; */
+  color: #2a4461;
+}
+</style>
